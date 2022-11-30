ce5bfdc3222db0356e7bd21bf80b6b25a3fd2ac5
Andrew <agardner@arizona.edu>
2022-11-30 13:07:29 -0700

    Add branches to setup

--- setup_chai_dev.sh
+++ setup_chai_dev.sh
@@ -31,9 +31,33 @@ else
   sudo chmod -R 0777 /etc/chai
 fi
 
-# Checkout branches.
-cd plugins/chai-src && git checkout main && cd -
-cd plugins/chai-src/proto && git checkout main && cd -
+# Checkout branches for milk.
 cd src/ImageStreamIO && git checkout dev && cd -
+
+# Checkout branches for plugins.
+RET_DIR=`pwd`
+cd plugins/cacao-addons/milk-zmq/           ; git checkout master ; cd $RET_DIR
+cd plugins/cacao-addons/mxlib/              ; git checkout main   ; cd $RET_DIR
+cd plugins/cacao-addons/rtimv/              ; git checkout main   ; cd $RET_DIR
+cd plugins/cacao-addons/xrif/               ; git checkout main   ; cd $RET_DIR
+cd plugins/cacao-src/                       ; git checkout dev    ; cd $RET_DIR
+cd plugins/chai-src/                        ; git checkout main   ; cd $RET_DIR
+cd plugins/chai-src/proto/                  ; git checkout main   ; cd $RET_DIR
+cd plugins/libs/libtar/                     ; git checkout master ; cd $RET_DIR
+cd plugins/milk-extra-src/clustering/       ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/fft/              ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/image_basic/      ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/image_filter/     ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/image_format/     ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/image_gen/        ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/img_reduce/       ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/info/             ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/kdtree/           ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/linARfilterPred/  ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/linopt_imtools/   ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/psf/              ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/statistic/        ; git checkout dev    ; cd $RET_DIR
+cd plugins/milk-extra-src/ZernikePolyn/     ; git checkout dev    ; cd $RET_DIR
+cd plugins/mmtao-main/mmtao-main/           ; git checkout main   ; cd $RET_DIR
 
 echo "Done."
