5f7c9df8ce4325985ac5d24afaac64798565ff5f
Andrew Gardner <agardner@arizona.eduw>
2023-09-15 12:30:56 -0700

    Sync submodules

--- setup_chai_dev.sh
+++ setup_chai_dev.sh
@@ -42,20 +42,6 @@ cd plugins/cacao-addons/rtimv/              ; git checkout main   ; git config c
 cd plugins/cacao-addons/xrif/               ; git checkout main   ; git config core.fileMode false; git pull; cd $RET_DIR
 cd plugins/chai-src/                        ; git checkout main   ; git config core.fileMode false; git pull; cd $RET_DIR
 cd plugins/chai-src/proto/                  ; git checkout main   ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/clustering/       ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/fft/              ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/image_basic/      ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/image_filter/     ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/image_format/     ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/image_gen/        ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/img_reduce/       ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/info/             ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/kdtree/           ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/linARfilterPred/  ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/linopt_imtools/   ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/psf/              ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/statistic/        ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
-cd plugins/milk-extra-src/ZernikePolyn/     ; git checkout dev    ; git config core.fileMode false; git pull; cd $RET_DIR
 cd plugins/mmtao-main/mmtao-main/           ; git checkout main   ; git config core.fileMode false; git pull; cd $RET_DIR
 
 echo "Done."
